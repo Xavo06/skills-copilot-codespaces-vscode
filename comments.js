e28591ed80a5ee3e37d553e57fe290f1006bbb1d
Xavo06 <150401624+Xavo06@users.noreply.github.com>
2023-11-13 19:16:13 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,16 @@
+// Create web server
+// Create API
+// Create a new comment
+// Read all comments
+// Read a comment by id
+// Update a comment by id
+// Delete a comment by id
+// Start server
+// Connect to database
+// Create a Comment model
+// Create a Comment schema
+// Create a Comment instance
+// Create a Comment instance
+// Create a Comment instance
+// Create a Comment instance
+// Create a
